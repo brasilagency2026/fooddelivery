0fc0c4aaa994a17acd77a36bd17a8fd76a01b882
gleveque27 <glwebagency2@gmail.com>
2026-05-23 15:49:23 -0300

    Fix: remove destructive RedirectToSignIn from AdminLayout that was causing infinite loops and deprecated redirectUrl errors on the login page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,3 @@
-import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
-
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <SignedIn>{children}</SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
-  );
+  return <>{children}</>;
 }
