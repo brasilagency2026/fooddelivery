f5a13cca71c0318b4913020c836e26f7e2330c8d
gleveque27 <glwebagency2@gmail.com>
2026-05-24 16:17:40 -0300

    SEO: Add robots.ts to allow Facebook scraper

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://delivery.foodpronto.com.br/sitemap.xml',
+  }
+}
